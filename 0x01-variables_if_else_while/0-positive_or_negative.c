b8c614871242753fb5956e8b0313e063ef281586
Hazemadly01 <hazemadly01@gmail.com>
2023-10-12 15:31:23 -0700

    C commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,6 +16,7 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 if (n > 0)
 printf("%i is positve\n", n);
 
@@ -24,5 +25,6 @@ printf("%i is zero\n", n);
 
 else
 printf("%i is negative\n", n);
+
 return (0);
 }
