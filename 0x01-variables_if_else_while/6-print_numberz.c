72be794c425bc4d612188371474daf5bb345dee6
Hazemadly01 <hazemadly01@gmail.com>
2023-10-12 09:35:09 -0700

    C commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,17 +4,19 @@
  * main - entry point
  *
  * description: print 0-9 using putchar
- *wile using int varible
+ * wile using int varible
  *
  * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 int digit = 0;
-{
+
 while (digit <= 9)
+{
 putchr(digit + '0');
-digit++
+digit++;
 }
 putchar('\n');
 
