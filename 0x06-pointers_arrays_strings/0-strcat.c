3dffe417288f320a147f51dcb054eb9b08ca7f7a
Hazemadly01 <hazemadly01@gmail.com>
2023-10-19 11:16:02 -0700

    new commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,34 @@
 #include "main.h"
 
 /**
- * _strcat - funcation that concatenates
+ * _strcat - function that concatenates
  * two strings
  *
- * @dest: pointer to destnation input
- * @src: pointr to sorce input
+ * @dest: pointer to destination input
+ * @src: pointer to source input
  *
- * retrun: pointer to resulting string @dest
+ * Return: pointer to resulting string @dest
 */
 
-char *_stract(char *dest, char *src)
+char *_strcat(char *dest, const char *src)
 {
 int c, c2;
 
 c = 0;
-/* find the sixe of dast arrays */
+/* find the size of dest array */
 while (dest[c])
 c++;
 
-/* iterate throught each src array value without the null byte */
-for (c2 = 0; src[c2] ; c++)
+/* iterate through each src array value without the null byte */
+for (c2 = 0; src[c2]; c2++)
+{
 /* append src[c2] to dest[c] while overwriting the null byte in dest */
-dest[c++] = src[c2];
+dest[c] = src[c2];
+c++;
+}
+
+/* add null terminator at the end of the concatenated string */
+dest[c] = '\0';
 
 return (dest);
 }
