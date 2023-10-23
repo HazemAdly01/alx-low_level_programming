e98299abeefa0fd0c65913acfa4870abe55ab453
Hazemadly01 <hazemadly01@gmail.com>
2023-10-23 05:11:35 -0700

    new commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,34 +1,26 @@
-#include "main.h"
-
 /**
  * _strcat - function that concatenates
- * two strings
+ *          two strings.
  *
- * @dest: pointer to destination input
+ * @dest: pointer to destnation input
  * @src: pointer to source input
  *
  * Return: pointer to resulting string @dest
 */
 
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-int c, c2;
+	int c, c2;
 
-c = 0;
-/* find the size of dest array */
-while (dest[c])
-c++;
+	c = 0;
+	/*find the size of dest array*/
+	while (dest[c])
+		c++;
 
-/* iterate through each src array value without the null byte */
-for (c2 = 0; src[c2]; c2++)
-{
-/* append src[c2] to dest[c] while overwriting the null byte in dest */
-dest[c] = src[c2];
-c++;
-}
+	/* iterate through each src array value without the null byte*/
+	for (c2 = 0; src[c2] ; c2++)
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+		dest[c++] = src[c2];
 
-/* add null terminator at the end of the concatenated string */
-dest[c] = '\0';
-
-return (dest);
+	return (dest);
 }
